a5bb0793e985589a752bb61e2fde44d28102d166
hociatec <contact@hociatec.fr>
2026-01-26 16:57:03 +0100

    j

--- client-win/client-win/MainWindow.xaml.cs
+++ client-win/client-win/MainWindow.xaml.cs
@@ -2,6 +2,8 @@
 using System;
 using System.Threading.Tasks;
 using System.Windows;
+using System.Windows.Automation.Peers;
+using System.Windows.Automation.Provider;
 using System.Windows.Input;
 using System.Windows.Interop;
 using System.Windows.Threading;
@@ -15,6 +17,10 @@ namespace client_win
         public MainWindow()
         {
             InitializeComponent();
+            
+            // Maximiser la fenêtre au démarrage
+            WindowState = WindowState.Maximized;
+            
             Loaded += OnLoaded;
         }
 
@@ -22,13 +28,12 @@ namespace client_win
         {
             if (_didStartupFocusNudge)
                 return;
-
             _didStartupFocusNudge = true;
 
             // Attendre que la fenêtre soit complètement chargée
-            await Task.Delay(100);
+            await Task.Delay(150);
 
-            Dispatcher.BeginInvoke(DispatcherPriority.ApplicationIdle, () =>
+            await Dispatcher.InvokeAsync(() =>
             {
                 try
                 {
@@ -44,18 +49,87 @@ namespace client_win
                         NativeMethods.SetForegroundWindow(helper.Handle);
                     }
 
-                    // Positionner le focus sur le premier élément interactif
-
-                    if (!IsKeyboardFocusWithin)
+                    // Attendre un instant supplémentaire pour la stabilisation
+                    Task.Delay(50).ContinueWith(_ =>
                     {
-                        MoveFocus(new TraversalRequest(FocusNavigationDirection.First));
-                    }
+                        Dispatcher.Invoke(() =>
+                        {
+                            // Positionner le focus sur le premier élément interactif
+                            var firstFocusable = FindFirstFocusableElement();
+                            if (firstFocusable != null)
+                            {
+                                firstFocusable.Focus();
+                                Keyboard.Focus(firstFocusable);
+                                
+                                // Notifier les lecteurs d'écran
+                                NotifyScreenReader(firstFocusable);
+                            }
+                            else
+                            {
+                                MoveFocus(new TraversalRequest(FocusNavigationDirection.First));
+                            }
+                        });
+                    });
                 }
                 catch
                 {
                     // Ignorer les erreurs (best-effort)
                 }
-            });
+            }, DispatcherPriority.ApplicationIdle);
+        }
+
+        private IInputElement FindFirstFocusableElement()
+        {
+            // Rechercher le premier élément focusable dans l'arborescence visuelle
+            return FocusManager.GetFocusedElement(this) as IInputElement 
+                   ?? PredictionServices.GetFirstFocusableChild(this);
+        }
+
+        private void NotifyScreenReader(IInputElement element)
+        {
+            try
+            {
+                if (element is UIElement uiElement)
+                {
+                    var peer = UIElementAutomationPeer.CreatePeerForElement(uiElement);
+                    if (peer != null)
+                    {
+                        peer.RaiseAutomationEvent(AutomationEvents.AutomationFocusChanged);
+                        
+                        // Si l'élément supporte l'invocation, notifier également
+                        if (peer.GetPattern(PatternInterface.Invoke) is IInvokeProvider)
+                        {
+                            peer.RaiseAutomationEvent(AutomationEvents.LiveRegionChanged);
+                        }
+                    }
+                }
+            }
+            catch
+            {
+                // Ignorer les erreurs
+            }
+        }
+    }
+
+    // Classe helper pour trouver le premier élément focusable
+    internal static class PredictionServices
+    {
+        public static IInputElement GetFirstFocusableChild(DependencyObject parent)
+        {
+            if (parent is UIElement element && element.Focusable && element.IsEnabled && element.Visibility == Visibility.Visible)
+            {
+                return element;
+            }
+
+            for (int i = 0; i < System.Windows.Media.VisualTreeHelper.GetChildrenCount(parent); i++)
+            {
+                var child = System.Windows.Media.VisualTreeHelper.GetChild(parent, i);
+                var result = GetFirstFocusableChild(child);
+                if (result != null)
+                    return result;
+            }
+
+            return null;
         }
     }
 }
